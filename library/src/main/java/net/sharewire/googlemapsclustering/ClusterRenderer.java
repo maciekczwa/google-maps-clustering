8212c739c443dbc58093978efe3872d4ef960edc
Maciej Czerniak <maciek.czerniak@looksoft.pl>
2020-12-11 13:21:48 +0100

    fix build

--- library/src/main/java/net/sharewire/googlemapsclustering/ClusterRenderer.java
+++ library/src/main/java/net/sharewire/googlemapsclustering/ClusterRenderer.java
@@ -7,7 +7,8 @@ import android.animation.TypeEvaluator;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.view.animation.FastOutSlowInInterpolator;
+
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator;
 
 import java.util.ArrayList;
 import java.util.HashMap;
